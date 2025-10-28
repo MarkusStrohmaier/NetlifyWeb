f8a5f1082d06559b3a2925c2e37533bef9fdddea
MarkusStrohmaier <heliumbalonexperiment1@gmail.com>
2025-10-28 16:20:43 +0100

    22

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { EtfSavingsPlanCalculator } from "./components/EtfSavingsPlanCalculator";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 dark:from-gray-900 dark:to-gray-800 p-4 md:p-8">
-      <EtfSavingsPlanCalculator />
-    </div>
-  );
-}
